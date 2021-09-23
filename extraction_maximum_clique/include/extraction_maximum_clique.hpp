1058e4b10dba80263b254c3f1349c95c8e17e01f
Sota Tsuji <tuzisou201p@gmail.com>
2021-09-23 09:22:15 +0900

    [clean] ファイル・フォルダの整理およびリファクタリング

--- extraction_maximum_clique/include/extraction_maximum_clique.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDED_extraction_maximum_clique_hpp_
-#define INCLUDED_extraction_maximum_clique_hpp_
-
-#include "weight_graph.hpp"
-
-bool is_complete_graph(const WeightGraph& T);
-
-#endif
